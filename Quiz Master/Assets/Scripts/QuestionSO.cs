f7a934d48434d9bfb9e356b5415d65f4c52ea6ea
Nathan Lunn <nathandouglaslunn@gmail.com>
2023-04-02 07:01:54 -0400

    create two getter methods, one that gets the correct answer index given in the serialized field, and another that gets the correct answer from the array of answers based on that correct answer index

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -8,9 +8,20 @@ public class QuestionSO : ScriptableObject
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter New Question Text Here";
     [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
     public string GetQuestion()
     {
         return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
     }
 }
