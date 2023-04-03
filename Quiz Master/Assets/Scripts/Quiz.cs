aed2d7bf800f8b871908c74bd4f89938cf07aac9
Nathan Lunn <nathandouglaslunn@gmail.com>
2023-04-03 06:45:24 -0400

    repeat the process of accessing the TMPUGUI objects in a for loop in order to input the correct text of each answer

--- Quiz Master/Assets/Scripts/Quiz.cs
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -9,14 +9,15 @@ public class Quiz : MonoBehaviour
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
 
+
     void Start()
     {
         questionText.text = question.GetQuestion();
 
         for (int i = 0; i < answerButtons.Length; i++)
         {
-            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = question.GetAnswer(i);
+            TextMeshProUGUI answerTMP = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            answerTMP.text = question.GetAnswer(i);
         }
     }
 }
